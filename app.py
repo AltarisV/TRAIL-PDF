c9234a5207c72f1e073bb7d00d4f99139042bf96
SpiegelV <leon.schuessler@freenet.de>
2024-02-11 22:24:51 +0100

    Add deletion functionality and merge changes implemented by https://github.com/DrawingNea in https://github.com/AltarisV/TRAIL-PDF/pull/2

--- app.py
+++ app.py
@@ -31,14 +31,15 @@ def index():
 
 @app.route('/', methods=['POST'])
 def upload_files():
-    uploaded_file = request.files['file']
-    filename = secure_filename(uploaded_file.filename)
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-            return "Invalid file type", 400
-        file_path = os.path.join(app.config['UPLOAD_PATH'], filename)
-        uploaded_file.save(file_path)
+    uploaded_files = request.files.getlist('file[]')
+    for uploaded_file in uploaded_files:
+        filename = secure_filename(uploaded_file.filename)
+        if filename != '':
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                return "Invalid file type", 400
+            file_path = os.path.join(app.config['UPLOAD_PATH'], filename)
+            uploaded_file.save(file_path)
 
     return redirect(url_for('index'))
 
@@ -46,6 +47,18 @@ def upload_files():
 @app.route('/uploads/<filename>')
 def upload(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+
+@app.route('/delete/<filename>')
+def delete_file(filename):
+    file_path = os.path.join(app.config['UPLOAD_PATH'], filename)
+    try:
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+    except Exception as e:
+        # Log the error here
+        print(e)
+    return redirect(url_for('index'))
 
 
 @app.route('/files/<filename>')
